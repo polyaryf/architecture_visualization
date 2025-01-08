9ff858b8ab6244b79112654d4f12099e39da630a
Polya Ryftina <90066843+polyaryf@users.noreply.github.com>
2025-01-08 15:11:07 +0300

    Adding project with podfile (#1)
    * project added
    
    * added podfile

---
+++ ArchitectureVisualization/ArchitectureVisualization/ArchitectureVisualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureVisualizationApp.swift
+//  ArchitectureVisualization
+//
+//  Created by Полина Рыфтина on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureVisualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
