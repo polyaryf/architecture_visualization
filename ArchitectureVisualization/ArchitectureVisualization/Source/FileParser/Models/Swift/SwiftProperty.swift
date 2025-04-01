bcaf49a34424003f0c431455594019854c908d22
Polina Ryftina <pryftina@gmail.com>
2025-04-01 18:22:42 +0300

    interactive distribution view

--- ArchitectureVisualization/ArchitectureVisualization/Source/FileParser/Models/Swift/SwiftProperty.swift
+++ ArchitectureVisualization/ArchitectureVisualization/Source/FileParser/Models/Swift/SwiftProperty.swift
@@ -1,6 +1,12 @@
 import Foundation
 
-struct SwiftProperty: Equatable, Hashable {
+enum ReferenceModifier: String, Codable {
+    case strong, weak, unowned
+}
+
+struct SwiftProperty: Codable, Identifiable, Equatable, Hashable {
     let id = UUID()
     let name: String
+    let type: String
+    let modifier: ReferenceModifier
 }
